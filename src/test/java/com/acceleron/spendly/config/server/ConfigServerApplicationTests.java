2e6b416cac8b198fde6be8a1fb9e371c410880e4
Max Kutsepalov <maks.kutsepalov.70@gmail.com>
2022-10-11 11:22:09 +0300

    initial commit

---
+++ src/test/java/com/acceleron/spendly/config/server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acceleron.spendly.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
